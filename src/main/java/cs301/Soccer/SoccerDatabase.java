e02bdd40c3f71aee05482f4b578eaada880e92a9
Lindsey Warren <warren23@up.edu>
2020-09-30 22:10:01 -0700

    Initial commit

---
+++ src/main/java/cs301/Soccer/SoccerDatabase.java
@@ -0,0 +1,278 @@
+package cs301.Soccer;
+
+import android.util.Log;
+
+import cs301.Soccer.soccerPlayer.SoccerPlayer;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+/**
+ * Soccer player database -- presently, all dummied up
+ *
+ * @author *** put your name here ***
+ * @version *** put date of completion here ***
+ */
+public class SoccerDatabase implements SoccerDB {
+
+    Hashtable<String, SoccerPlayer> hashtable = new Hashtable<>();
+
+    /**
+     * add a player
+     *
+     * @see SoccerDB#addPlayer(String, String, int, String)
+     */
+    @Override
+    public boolean addPlayer(String firstName, String lastName,
+                             int uniformNumber, String teamName) {
+        if (hashtable.get(firstName + lastName) == null) {
+            SoccerPlayer player = new SoccerPlayer(firstName, lastName, uniformNumber, teamName);
+            hashtable.put(firstName + lastName, player);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * remove a player
+     *
+     * @see SoccerDB#removePlayer(String, String)
+     */
+    @Override
+    public boolean removePlayer(String firstName, String lastName) {
+        if (hashtable.get(firstName + lastName) != null) {
+            hashtable.remove(firstName + lastName);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * look up a player
+     *
+     * @see SoccerDB#getPlayer(String, String)
+     */
+    @Override
+    public SoccerPlayer getPlayer(String firstName, String lastName) {
+        SoccerPlayer player = hashtable.get(firstName + lastName);
+        if (player != null) {
+            return player;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * increment a player's goals
+     *
+     * @see SoccerDB#bumpGoals(String, String)
+     */
+    @Override
+    public boolean bumpGoals(String firstName, String lastName) {
+        SoccerPlayer player = hashtable.get(firstName + lastName);
+        if (player != null) {
+            player.bumpGoals();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * increment a player's assists
+     *
+     * @see SoccerDB#bumpAssists(String, String)
+     */
+    @Override
+    public boolean bumpAssists(String firstName, String lastName) {
+        SoccerPlayer player = hashtable.get(firstName + lastName);
+        if (player != null) {
+            player.bumpAssists();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * increment a player's shots
+     *
+     * @see SoccerDB#bumpShots(String, String)
+     */
+    @Override
+    public boolean bumpShots(String firstName, String lastName) {
+        SoccerPlayer player = hashtable.get(firstName + lastName);
+        if (player != null) {
+            player.bumpShots();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * increment a player's saves
+     *
+     * @see SoccerDB#bumpSaves(String, String)
+     */
+    @Override
+    public boolean bumpSaves(String firstName, String lastName) {
+        SoccerPlayer player = hashtable.get(firstName + lastName);
+        if (player != null) {
+            player.bumpSaves();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * increment a player's fouls
+     *
+     * @see SoccerDB#bumpFouls(String, String)
+     */
+    @Override
+    public boolean bumpFouls(String firstName, String lastName) {
+        SoccerPlayer player = hashtable.get(firstName + lastName);
+        if (player != null) {
+            player.bumpFouls();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * increment a player's yellow cards
+     *
+     * @see SoccerDB#bumpYellowCards(String, String)
+     */
+    @Override
+    public boolean bumpYellowCards(String firstName, String lastName) {
+        SoccerPlayer player = hashtable.get(firstName + lastName);
+        if (player != null) {
+            player.bumpYellowCards();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * increment a player's red cards
+     *
+     * @see SoccerDB#bumpRedCards(String, String)
+     */
+    @Override
+    public boolean bumpRedCards(String firstName, String lastName) {
+        SoccerPlayer player = hashtable.get(firstName + lastName);
+        if (player != null) {
+            player.bumpRedCards();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * tells the number of players on a given team
+     *
+     * @see SoccerDB#numPlayers(String)
+     */
+    @Override
+    // report number of players on a given team (or all players, if null)
+    public int numPlayers(String teamName) {
+        if (teamName == null) {
+            return hashtable.size();
+        }
+        else {
+            int size = 0;
+            Set<String> keys = hashtable.keySet();
+            for(String key : keys) {
+                if (hashtable.get(key).getTeamName().equals(teamName)) {
+                    size++;
+                }
+            }
+            return size;
+        }
+    }
+
+    /**
+     * gives the nth player on a the given team
+     *
+     * @see SoccerDB#playerNum(int, String)
+     */
+    // get the nTH player
+    @Override
+    public SoccerPlayer playerNum(int idx, String teamName) {
+        if (idx > numPlayers(teamName)) {
+           return null;
+        }
+        Set<String> keys = hashtable.keySet();
+        ArrayList<String> playerKeys = new ArrayList<String>();
+        if (teamName == null) {
+            for (String key: keys) {
+                playerKeys.add(key);
+            }
+        }
+        else { // teamName is not null
+            for (String key: keys) {
+                if (hashtable.get(key).getTeamName().equals(teamName)) {
+                    playerKeys.add(key);
+                }
+            }
+        }
+        SoccerPlayer player = hashtable.get(playerKeys.get(idx));
+        return player;
+    }
+
+
+    /**
+     * reads database data from a file
+     *
+     * @see SoccerDB#readData(java.io.File)
+     */
+    // read data from file
+    @Override
+    public boolean readData(File file) {
+        return file.exists();
+    }
+
+    /**
+     * write database data to a file
+     *
+     * @see SoccerDB#writeData(java.io.File)
+     */
+    // write data to file
+    @Override
+    public boolean writeData(File file) {
+        return false;
+    }
+
+    /**
+     * helper method that logcat-logs a string, and then returns the string.
+     *
+     * @param s the string to log
+     * @return the string s, unchanged
+     */
+    private String logString(String s) {
+        Log.i("write string", s);
+        return s;
+    }
+
+    /**
+     * returns the list of team names in the database
+     *
+     * @see cs301.Soccer.SoccerDB#getTeams()
+     */
+    // return list of teams
+    @Override
+    public HashSet<String> getTeams() {
+        return new HashSet<String>();
+    }
+
+}
